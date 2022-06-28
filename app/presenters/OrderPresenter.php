5b0584b66096128dcf0a3a98bc992e400b1103f0
roman@cervenan.cz <rcervenan>
2022-06-28 13:28:51 +0200

    order presenter

---
+++ app/presenters/OrderPresenter.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Presenters;
+
+/**
+ * Description of OrderPresenter
+ *
+ * @author Roman
+ */
+class OrderPresenter extends BasePresenter 
+{
+    //put your code here
+}
